ce65dce3c30dea9b2c9e023e34d5c2a98f0b01e4
fencer <982039620@qq.com>
2022-08-24 11:29:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, Text, TouchableOpacity, View, ViewPropTypes} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 
 export const LABEL_POSITION = {
   RIGHT: 'right',
